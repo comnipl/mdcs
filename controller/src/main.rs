cd15c28924ca1dc4ac882d16abf13035a80a4864
LegitYew0108 <legitlegit0108@gmail.com>
2025-09-17 01:40:22 +0900

    feat: read configs

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+pub mod config;
+
+use clap::Parser;
+use config::Config;
+use tracing::{debug, info};
+
+#[tokio::main]
+async fn main() {
+    // tracing_subscriberの初期化
+    tracing_subscriber::fmt().init();
+    info!("MDCS Controller Created!");
+
+    // Configを環境変数またはコマンド引数から取得
+    let config = Config::parse();
+
+    // デバッグ用 Configを表示する
+    debug!("{:?}",config);
 }
